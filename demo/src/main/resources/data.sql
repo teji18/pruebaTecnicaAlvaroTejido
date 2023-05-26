78e4217e233582c863297e4d491922256f5aa865
Álvaro <alvaro.tejido18@gmail.com>
2023-05-26 10:53:42 +0200

    flyway para iniciar la bd

--- demo/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE BD_HEROES (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    name varchar(255),
-    city varchar(255),
-    superpower varchar(255)
-);
---
+++ demo/src/main/resources/db/migration/V1.1__create_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE BD_HEROES (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name varchar(255),
+    city varchar(255),
+    superpower varchar(255)
+);
+
+insert into BD_HEROES (id,name,city,superpower)values(97,'Batman','Gotham','');
+insert into BD_HEROES (id,name,city,superpower)values(98,'SpiderMan','Speed','Araña');
+insert into BD_HEROES (id,name,city,superpower)values(99,'Superman','Paya','Vuela');
